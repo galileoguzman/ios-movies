fad403250ecebf8d4004f0b7d97891194e1f7629
galileoguzman <galileoguzman@gmail.com>
2020-11-26 10:00:36 -0600

    Initial commit

---
+++ movies-ios/Screens/MoviesVC.swift
@@ -0,0 +1,29 @@
+//
+//  MoviesVC.swift
+//  movies-ios
+//
+//  Created by Galileo Guzman on 26/11/20.
+//
+
+import UIKit
+
+class MoviesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
